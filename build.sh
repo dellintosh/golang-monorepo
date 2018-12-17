c30fa04b5f844956156b3d5937e562c2c9f287eb
Justus Luthy <jluthy@coinlion.com>
2018-12-17 16:40:00 -0600

    Output docker version

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
 # tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 # mv /tmp/docker/* /usr/bin
-docker --version
+docker version
 
 ### Install awscli. Used for pushing container images to ECR.
 ### You may want to set these environment variables:
